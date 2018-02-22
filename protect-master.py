fd1e7b3624cec5130e42cfed929034d7ba49fa02
Watson <watsoncav@gmail.com>
2018-02-22 20:41:47 -0300

    correção da função de saturação do TC

--- protect-master.py
+++ protect-master.py
@@ -6,19 +6,21 @@ class TC(object):
 				name,
 				Ipn,
 				Isn,
-				classExat = None,
+				accuracyClass = None,
 				Isat = None):
-		self.name = name
+		self.name = name 						# Identificacao do TC
 		self.Ipn = Ipn							# Corrente nominal do primario do TC
 		self.Isn = Isn							# Corrente nominal do secundario do TC
-		self.rtc = float(Ipn/Isn)						# Relacao de transformacao do TC
-		self.classExat = classExat				# Classe de exatidao do TC (valores tipicos para TC 
+		self.rtc = float(Ipn/Isn)				# Relacao de transformacao do TC
+		self.accuracyClass = accuracyClass		# Classe de exatidao do TC (valores tipicos para TC 
 												# de protecao: 2.5/5.0/10.0)
 
 		if Isat is None:						# Corrente do primario que leva o TC ao estado de saturacao
-			self.Isat = 20*Ipn
+			self.Isat = 20*Ipn 					# Caso nao seja passada na criacao do objeto, assume 20*Ipn
 		else:
 			self.Isat = Isat
+
+		self.isSaturated = False				# Flag que indica se o TC entrou em estado de saturacao
 
 	def offSetCurrent(self,Ip):
 		'''
@@ -26,12 +28,27 @@ class TC(object):
 			e percorrido por uma corrente Ip.
 		'''
 
-		if self.Isat>Ip:
-			print "Nao saturou"
-			return float(Ip)/self.rtc
-		else:
-			print "Saturou"
+		if self.Isat<Ip or self.isSaturated:		# Testa se o TC ficou ou esta saturado
+			self.setSaturation()
 			return float(self.Isat)/self.rtc
+		else:
+			return float(Ip)/self.rtc
+			
+	
+	def setSaturation(self,saturation=True):
+		'''
+			Funcao que seta o estado do TC para saturado (saturation=True) ou
+			funcional (Saturation=False). Por padrao, se nenhum parametro eh 
+			passado, o TC sera saturado.
+		'''
+
+		if saturation:
+			print self.name+" saturou. Prever manutencao."
+			self.isSaturated = True				# Em caso de saturacao, eh exibido um alerta de manutencao
+												# do TC. Eh necessario setar o parametro isSaturated = False
+												# para que o TC tenha sua funcionalidade reestabelecida
+		else:
+			self.isSaturated = False		 	
 
 
 class IED(object):
@@ -89,15 +106,17 @@ class AdjustGroup(object):
 		
 		# ajustes de fase
 
-		self.ipk51 = ipk51						# ajuste da corrente de pickup da funcao 51
 		self.ipk50 = ipk50						# ajuste da corrente de pickup da funcao 50
+
+		self.ipk51 = ipk51						# ajuste da corrente de pickup da funcao 51
 		self.curveP = curveP					# tipo ('NI','MI' ou 'EI') de curva de atuacao da funcao 50 
 		self.dialP = dialP						# dial da curva de atuacao da funcao 50 de fase
 
 		# ajustes de neutro
 
-		self.ipk51N = ipk51N					# ajuste da corrente de pickup da funcao 51N
 		self.ipk50N = ipk50N					# ajuste da corrente de pickup da funcao 50N
+		
+		self.ipk51N = ipk51N					# ajuste da corrente de pickup da funcao 51N
 		self.curveN = curveN					# tipo ('NI','MI' ou 'EI') de curva de atuacao da funcao 50N
 		self.dialN = dialN						# dial da curva de atuacao da funcao 50N
 
@@ -127,6 +146,7 @@ class AdjustGroup(object):
 
 		return alpha,beta
 
+# Rotinas de teste
 
 tc1 = TC("TC1",500,5)
 ied = IED("IED1",3,dict(),tc1)
@@ -141,9 +161,16 @@ print "Nome do TC: " + tc2.name
 print "Corrente nominais do TC: " + str(tc2.Ipn)
 print "Corrente de saturacao do TC: " + str(tc2.Isat)
 
-ipList = range(0,5010)
+j=4990
+ipList = range(j,5010)
 isList = []
 
 for i in ipList:
-	isList.append(tc2.offSetCurrent(ipList[i]))
-	print "Ip=" + str(ipList[i]) + "A, Is=" + str(isList[i]) +"A"
+	isList.append(tc2.offSetCurrent(ipList[i-j]))
+	print "Ip=" + str(ipList[i-j]) + "A, Is=" + str(isList[i-j]) +"A"
+
+for i in ipList:
+	isList[i-j]=tc2.offSetCurrent(ipList[i-j])
+	print "Ip=" + str(ipList[i-j]) + "A, Is=" + str(isList[i-j]) +"A"
+
+
