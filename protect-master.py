3647cef6460510abd63ba227c00a328a85f7e17e
Watson <watsoncav@gmail.com>
2018-02-26 21:50:23 -0300

    associação de TCs

--- protect-master.py
+++ protect-master.py
@@ -1,11 +1,14 @@
-class TC(object):
+global breakerList = []
+global iedList = []
+
+class CT(object):
 	'''
 		Classe que modela o TC, ***
 	'''
 	def __init__(self,
 				name,
-				Ipn,
-				Isn,
+				Ipn = 500,
+				Isn = 5,
 				accuracyClass = None,
 				Isat = None):
 		self.name = name 						# Identificacao do TC
@@ -45,11 +48,9 @@ class TC(object):
 		if saturation:
 			print self.name+" saturou. Prever manutencao."
 			self.isSaturated = True				# Em caso de saturacao, eh exibido um alerta de manutencao
-												# do TC. Eh necessario setar o parametro isSaturated = False
-												# para que o TC tenha sua funcionalidade reestabelecida
-		else:
-			self.isSaturated = False		 	
-
+		else:									# do TC. Eh necessario setar o parametro isSaturated = False
+			self.isSaturated = False			# para que o TC tenha sua funcionalidade reestabelecida
+					 	
 
 class IED(object):
 	'''
@@ -59,7 +60,8 @@ class IED(object):
 				name,
 				nmax,
 				functions,
-				tc,
+				activeGroup=None,
+				ct=None,
 				limits=None):
 
 		# parametros do IED, passados na criacao do objeto
@@ -67,14 +69,22 @@ class IED(object):
 		self.name = name
 		self.functions = functions 				# dicionario com as funcoes habilitadas no ied
 		self.nmax = nmax						# numero maximo de grupos de ajustes
-		self.TC = tc 							# TC associado ao IED						
-							
-		self.activeGroup = 0	 				# variavel que indica o grupo de ajuste ativo (por 
-		                                        # padrao, 0: sem funcoes habilitadas)
+		
+		# Laco que associa os TCs de fase e neutro ao IED
+		if ct is None:
+			self.ct=[CT("TC A do "+self.name)]
+			self.ct.append(CT("TC B do "+self.name))
+			self.ct.append(CT("TC C do "+self.name))
+			self.ct.append(CT("TC N do "+self.name))
+		else:
+			self.ct = ct		
+
+		self.activeGroup = activeGroup			# objeto da classe AdjustGroup contendo o grupo de
+												# ajustes ativo do IED
 		self.BRKF = False 						# flag para habilitar/desabilitar falha de disjuntor
 		self.function_sens = None 				# flag que indica a funcao de protecao indicada no 
 												# painel do ied
-		self.corrente = float()					# inicializa a corrente do ied como zero
+		self.current = float()					# inicializa a corrente do ied como zero
 
 		if limits is not None:					# limites permitidos pelo rele
 
@@ -95,6 +105,43 @@ class IED(object):
 
 			tag = str(i+1)
 			self.groups[tag] = None
+
+	def setTrip(self):
+		self.trip = True
+
+	def reset(self):
+		self.trip = False
+		self.current = 0
+
+
+class Breaker(object):
+	'''
+		Classe que modela o disjuntor, ***
+	'''
+	def __init__(self,
+				name,
+				opState='NC',
+				relay=None):
+		
+		self.name = name 						# Nome do disjuntor
+		self.relay = relay						# Associacao do rele ao disjuntor
+		if opState is 'NO':						# Define o estado inicial do disjuntor pelo seu estado 
+			self.state = 'open' 				# normal de operacao: open quando 'NO'(normalmente aberto) 
+		else:									# 					  closed nos demais casos.
+			self.state = 'closed'				# Por padrao, opState recebe 'NC'
+	
+	def isOpen(self):
+		if self.state = 'open':
+			return True
+		else:
+			return False
+
+	def setOpen(self):
+		self.state = 'open'
+
+	def setClosed(self):
+		self.state = 'closed' 			
+
 
 class AdjustGroup(object):
 	'''
@@ -146,31 +193,54 @@ class AdjustGroup(object):
 
 		return alpha,beta
 
+	def tripF50(self,Iphase):
+		if self.ipk50 > Iphase:
+			return False
+		else:
+			return True
+
+	def timeF51(self,Iphase):
+			a,b = self.returnParameters(self.curveP)
+			return self.dialP*b/(((Iphase/self.ipk50)**a)-1)	
+
+	def tripF50N(self,Ineutral):
+		if self.ipk50N > Ineutral:
+			return False
+		else:
+			return True
+
+	def timeF51N(self,Ineutral):
+		a,b = self.returnParameters(self.curveP)
+			return self.dialP*b/(((Iphase/self.ipk50)**a)-1)
+
+
+
 # Rotinas de teste
 
-tc1 = TC("TC1",500,5)
-ied = IED("IED1",3,dict(),tc1)
+##tcs = [CT("TCA",500,5),CT("TCB",500,5),CT("TCC",500,5),CT("TCN",50,5)]
+#ied = IED("IED1",3,dict(),tcs)
 
-print "Nome do IED: " + ied.name
-print "Nome do TC: " + tc1.name
-print "Corrente nominais do TC: " + str(tc1.Ipn)
-print "Corrente de saturacao do TC: " + str(tc1.Isat)
+#print "Nome do IED: " + ied.name
+#print "Nome do TC: " + ied.ct[1].name
+#print "Corrente nominais do TC: " + str(tcs[1].Ipn)
+#print "Corrente de saturacao do TC: " + str(tcs[1].Isat)
 
-tc2 = TC("TC2",500,1,2.5,5000)
-print "Nome do TC: " + tc2.name
-print "Corrente nominais do TC: " + str(tc2.Ipn)
-print "Corrente de saturacao do TC: " + str(tc2.Isat)
+#tc2 = CT("TC2",500,1,2.5,5000)
+#tcs[2] = tc2 
+#print "Nome do TC: " + ied.ct[2].name
+#print "Corrente nominais do TC: " + str(tcs[2].Ipn)
+#print "Corrente de saturacao do TC: " + str(tcs[2].Isat)
 
-j=4990
-ipList = range(j,5010)
-isList = []
+#j=4990
+#ipList = range(j,5010)
+#isList = []
 
-for i in ipList:
-	isList.append(tc2.offSetCurrent(ipList[i-j]))
-	print "Ip=" + str(ipList[i-j]) + "A, Is=" + str(isList[i-j]) +"A"
+#for i in ipList:
+#	isList.append(tc2.offSetCurrent(ipList[i-j]))
+#	print "Ip=" + str(ipList[i-j]) + "A, Is=" + str(isList[i-j]) +"A"
 
-for i in ipList:
-	isList[i-j]=tc2.offSetCurrent(ipList[i-j])
-	print "Ip=" + str(ipList[i-j]) + "A, Is=" + str(isList[i-j]) +"A"
+#for i in ipList:
+#	isList[i-j]=tc2.offSetCurrent(ipList[i-j])
+#	print "Ip=" + str(ipList[i-j]) + "A, Is=" + str(isList[i-j]) +"A"
 
 
